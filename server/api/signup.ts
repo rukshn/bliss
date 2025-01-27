cf5763ffa73f344ef2f2f8ed4212d7e88ce880a7
Rukshan <arkruka@gmail.com>
2025-01-27 08:14:30 +0100

    fix: :bug: hash user password with argon2

--- server/api/signup.ts
+++ server/api/signup.ts
@@ -1,23 +1,35 @@
-import { defineEventHandler, readBody } from 'h3';
-import {PrismaClient} from '@prisma/client';
+import { defineEventHandler, readBody } from "h3";
+import { PrismaClient } from "@prisma/client";
+import argon2 from "argon2";
 
-const createUser = async(name: string,  email: string, password:string) => {
-    const prisma = new PrismaClient() 
-    const user = await prisma.user.create({
-        data:{ 
-            name: name,
-            email: email,
-            password: password
-        }
-    }).finally(async () => {
-        await prisma.$disconnect()
-    })
+const createUser = async (name: string, email: string, password: string) => {
+  const prisma = new PrismaClient();
+  try {
+    const user = await prisma.user
+      .create({
+        data: {
+          name: name,
+          email: email,
+          password: await argon2.hash(password),
+        },
+      })
+      .finally(async () => {
+        await prisma.$disconnect();
+      });
 
-    return user
-}
+    return {
+      name: user.name,
+      email: user.email,
+      id: user.id,
+      githubId: user.githubId,
+    };
+  } catch (error) {
+    return { error: "an unexpected error occrued" };
+  }
+};
 
 export default defineEventHandler(async (event) => {
-    const body = await readBody(event)
-    const user = await createUser(body.name, body.email, body.password)
-    return {data: user}
-})
+  const body = await readBody(event);
+  const user = await createUser(body.name, body.email, body.password);
+  return { data: user };
+});
