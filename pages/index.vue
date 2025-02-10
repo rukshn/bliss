c053f8fdf45d74b8a13e6f2823ee92718ac98492
Rukshan <arkruka@gmail.com>
2025-02-10 08:42:51 +0100

    style: :lipstick: expand content area

--- pages/index.vue
+++ pages/index.vue
@@ -20,14 +20,24 @@ const toggleEditor = () => {
 };
 
 const submitPost = () => {
+  if (!store.open) {
+    store.open = true;
+    return;
+  }
   const jwt = useCookie("jwt");
   if (store.title.trim().length === 0) {
+    store.message = "Title is required";
+    store.hasMessage = true;
     return;
   }
   if (store.editor.trim().length === 0) {
+    store.message = "Post is required";
+    store.hasMessage = true;
     return;
   }
   if (chStore.channel.id === 0 || !chStore.channel.id) {
+    store.message = "Please select a channel";
+    store.hasMessage = true;
     return;
   }
   fetch("/api/post/create", {
@@ -41,19 +51,40 @@ const submitPost = () => {
       post: store.editor,
       channelId: chStore.channel.id,
     }),
-  }).then((response) => {
-    if (response.ok) {
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+    })
+    .then((data) => {
+      feed.feed.unshift({
+        title: store.title,
+        uuid: data.data,
+        createdBy: {
+          username: user.user.name,
+          profilePicture: user.user.avatar,
+        },
+        createdAt: new Date(),
+        channel: {
+          id: chStore.channel.id,
+          name: chStore.channel.title,
+        },
+        comments: 0,
+      });
       store.title = "";
       store.editor = "";
       store.open = false;
-    }
-  });
+      store.message = "";
+      store.hasMessage = false;
+    });
 };
 </script>
 <template>
-  <div class="px-4 sm:px-6 lg:px-8 py-8 border-l min-h-full">
-    <h1 class="font-bold text-3xl">{{ chStore.channel.title }}</h1>
-    <div class="my-6">
+  <div class="border-l py-8">
+    <h1 class="font-bold text-3xl px-6">{{ chStore.channel.title }}</h1>
+    <p class="text-xs text-gray-500 px-6">{{ chStore.channel.description }}</p>
+    <div class="p-6">
       <div class="bg-gray-100 py-3 rounded-lg px-3">
         <div class="flex items-center gap-3">
           <Avatar>
@@ -75,7 +106,7 @@ const submitPost = () => {
         <Editor class="py-3" v-if="store.open"></Editor>
       </div>
     </div>
-    <ScrollArea class="my-6 grid h-[calc(100vh-200px)]">
+    <ScrollArea class="px-6 grid h-[calc(100vh-190px)]">
       <PostCard v-for="(post, index) in feed.feed" :key="index" :post="post" />
     </ScrollArea>
   </div>
